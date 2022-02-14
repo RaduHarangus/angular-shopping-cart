3fdc936c9b340c4ea56e8ac1e23b9afabe4662f1
Radu H <52208792+Radoogle@users.noreply.github.com>
2022-02-14 16:30:52 +0200

    progress save

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from "../../services/cart.service";
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  public totalItems: number = 0;
+  public searchTerm: string = '';
+
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.cartService.getProducts()
+      .subscribe(res => {
+        this.totalItems = res.length;
+      }
+    );
+  }
+
+  search(event: any) {
+    this.searchTerm = (event.target as HTMLInputElement).value;
+    this.cartService.search.next(this.searchTerm);
   }
 
 }
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-products',
@@ -9,16 +10,32 @@ import { ApiService } from '../../services/api.service';
 export class ProductsComponent implements OnInit {
 
   public productList: any = [];
+  searchKey: string = '';
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService,
+              private cartService: CartService) { }
 
   ngOnInit(): void {
     this.api.getProducts()
       .subscribe(res => {
         this.productList = res;
+        this.productList.forEach((a: any) => {
+          Object.assign(a, {quantity: 1, total: a.price});
+        });
       }, err => {
         alert("There was a problem getting data from the server!")
       });
+    this.cartService.search.subscribe((val: any) => {
+      this.searchKey = val;
+    });
+  }
+
+  addToCart(item: any) {
+    this.cartService.addToCart(item);
+  }
+
+  filterElectronics() {
+    this.productList.filter((item:any) => item.category === "electronics");
   }
 
 }
